e989f30b1db92c9b4ea10125b49a263b2e71d98d
Fausto Morales <fausto@robinbay.com>
2022-06-14 11:04:08 -0500

    Upgrade storybook components.

--- qslwidgets/.storybook/main.js
+++ qslwidgets/.storybook/main.js
@@ -1,7 +1,12 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [],
-  features: { emotionAlias: false },
+  features: {
+    emotionAlias: false
+  },
   framework: "@storybook/react",
   staticDirs: ["../../qsl/testing/data"],
+  core: {
+    builder: "webpack5"
+  }
 };
